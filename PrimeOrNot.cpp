2ac991ba967b089c710453e6809d1256e9a33a08
Mayank Singour <114223386+MayankSingour@users.noreply.github.com>
2022-10-05 12:43:58 +0530

    Update PrimeOrNot.cpp

--- PrimeOrNot.cpp
+++ PrimeOrNot.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
 using namespace std;
 
-
 int main() {
-	int t;
-	cin>>t;
-	while(t--){
-	    int n,c=0;
-	    cin>>n;
-	    
-	    if(n==0 || n==1){
-	        cout<<"No"<<endl;
-	    }else{
-	        for(int i=1; i<n; i++){
-	            if(n%i==0){
-	                c++;
-	            }
-	        }
-	        if(c>=2){
-	            cout<<"No"<<endl;
-	        }
-	        else{
-	            cout<<"Yes"<<endl;
-	        }
-	    }
-	    
-	    
-	}
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,c=0;
+        cin>>n;
+        if(n==0||n==1)
+        {
+            cout<<"no"<<endl;
+            
+        }else{
+        for(int i=1;i<n;i++)
+        {
+            if(n%i==0)
+            c++;
+        }if(c>=2)
+        {
+            cout<<"no"<<endl;
+            
+        }else
+        cout<<"yes"<<endl;
+        }
+    }
 	
 	return 0;
 }
